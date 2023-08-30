21cc43c46da0382632e1bf46ebdd4429dc40b7b6
WD1120 <104724266+WD1120@users.noreply.github.com>
2023-08-30 15:16:59 +1000

    updated dataset

--- eval.py
+++ eval.py
@@ -31,7 +31,7 @@ def calcPL(prcHist):
     value = 0
     todayPLL = []
     (_,nt) = prcHist.shape
-    for t in range(251,501): 
+    for t in range(501,751): 
         prcHistSoFar = prcHist[:,:t]
         newPosOrig = getPosition(prcHistSoFar)
         curPrices = prcHistSoFar[:,-1] #prcHist[:,t-1]
